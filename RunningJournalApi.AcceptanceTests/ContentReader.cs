36d6754cb86f41d1717cd312e92317a21b6359cb
Mark Seemann <mark@seemann.ms>
2012-10-03 11:36:17 +0200

    Added ReadAsJsonAsync and ReadAsXml extensions.

---
+++ RunningJournalApi.AcceptanceTests/ContentReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Xml.Linq;
+
+namespace Ploeh.Samples.RunningJournalApi.AcceptanceTests
+{
+    public static class ContentReader
+    {
+        public static Task<dynamic> ReadAsJsonAsync(this HttpContent content)
+        {
+            if (content == null)
+                throw new ArgumentNullException("content");
+
+            return content.ReadAsStringAsync().ContinueWith(t =>
+                JsonConvert.DeserializeObject(t.Result));
+        }
+
+        public static Task<XDocument> ReadAsXmlAsync(this HttpContent content)
+        {
+            if (content == null)
+                throw new ArgumentNullException("content");
+
+            return content.ReadAsStringAsync().ContinueWith(t =>
+                XDocument.Parse(t.Result));
+        }
+    }
+}
---
+++ RunningJournalApi.AcceptanceTests/JsonContent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace Ploeh.Samples.RunningJournalApi.AcceptanceTests
+{
+    public class JsonContent : StringContent
+    {
+        public JsonContent(object value)
+            : base(SerializeToJson(value))
+        {
+        }
+
+        private static string SerializeToJson(object value)
+        {
+            return JsonConvert.SerializeObject(value);
+        }
+    }
+}
